9c97d1e886cce15d1d8e1bcde5a911095e7601ba
Franso <francismuasya@gmail.com>
2022-07-21 10:50:26 +0300

    create modal pop ups

--- src/components/entries.components.js
+++ src/components/entries.components.js
@@ -48,45 +48,110 @@ const Entries = () => {
             />
           ))}
       </Container>
+
+      <Modal show={addNewEntry} onHide={() => setAddNewEntry(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Calorie Entry</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>dish</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                newEntry.dish = event.target.value;
+              }}
+            ></Form.Control>
+            <Form.Label>ingredient</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                newEntry.ingredients = event.target.value;
+              }}
+            ></Form.Control>
+
+            <Form.Label>calorie</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                newEntry.calories = event.target.value;
+              }}
+            ></Form.Control>
+
+            <Form.Label>fat</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                newEntry.fat = event.target.value;
+              }}
+            ></Form.Control>
+          </Form.Group>
+          <Button onClick={() => addSingleEntry()}>Add</Button>
+          <Button onClick={() => setAddNewEntry(false)}>Cancel</Button>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        show={changeIngredient.change}
+        onHide={() => setChangeIngredient({ change: false, id: 0 })}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Change Ingredients</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label> New Ingredients</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                setNewIngredientName(event.target.value);
+              }}
+            ></Form.Control>
+            <Button onClick={() => changeIngredientForEntry()}>Change</Button>
+            <Button
+              onClick={() => setChangeIngredient({ change: false, id: 0 })}
+            >
+              Cancel
+            </Button>
+          </Form.Group>
+        </Modal.Body>
+      </Modal>
     </div>
   );
-};
 
-function addSingleEntry() {
-  setAddNewEntry(false);
-  var url = "http://localhost:8000/entry/create";
-  axios
-    .post(url, {
-      ingredients: newEntry.ingredients,
-      dish: newEntry.dish,
-      calorie: newEntry.calorie,
-      fat: parseFloat(newEntry.fat),
-    })
-    .then((response) => {
+  function addSingleEntry() {
+    setAddNewEntry(false);
+    var url = "http://localhost:8000/entry/create";
+    axios
+      .post(url, {
+        ingredients: newEntry.ingredients,
+        dish: newEntry.dish,
+        calorie: newEntry.calorie,
+        fat: parseFloat(newEntry.fat),
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          setRefreshData(true);
+        }
+      });
+  }
+
+  function deleteSingleEntry(id) {
+    var url = "http:localhost:8000/entries/delete" + id;
+    axios.delete(url, {}).then((response) => {
       if (response.status == 200) {
         setRefreshData(true);
       }
     });
-}
+  }
 
-function deleteSingleEntry(id) {
-  var url = "http:localhost:8000/entries/delete" + id;
-  axios.delete(url, {}).then((response) => {
-    if (response.status == 200) {
-      setRefreshData(true);
-    }
-  });
-}
-
-function getAllEntries() {
-  var url = "http://localhost:8000/entries";
-  axios
-    .get(url, {
-      responseType: "json",
-    })
-    .then((response) => {
-      if (response.status == 200) {
-        setEntries(response.data);
-      }
-    });
-}
+  function getAllEntries() {
+    var url = "http://localhost:8000/entries";
+    axios
+      .get(url, {
+        responseType: "json",
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          setEntries(response.data);
+        }
+      });
+  }
+};
