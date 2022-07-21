53c03b2a070661698c80566bc5cd5424a074d9b2
Franso <francismuasya@gmail.com>
2022-07-21 10:04:48 +0300

    create entry component

---
+++ src/components/entries.components.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Form, Container, Modal } from "react-bootstrap";
+
+import Entry from "./single-entry.component";
+
+function Entries() {
+  return <div> Entries</div>;
+}
+
+export default Entries;
